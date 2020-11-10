80ddc30eb35db5dbf473b103ac44567262ee6056
shjinyuan <jinyuanqq@163.com>
2020-11-11 07:41:53 +0800

    vim trainning doc

---
+++ vim/vim_practice.cpp
@@ -0,0 +1,68 @@
+/*
+ * Filename: vim_practice.cpp
+ *
+ * Copyright (C) 2018-2023 yuan  <yuan@company.com>. All Rights Reserved.
+ *
+ * History:
+ *    2020/10/14 - [yuan] Created file
+ *
+ * Maintainer: yuan <yuan@email.com>
+ *    Created: 2020-10-14
+ * LastChange: 2020-10-14
+ *    Version: v0.0.01
+ *
+ */
+
+#include <iostream>
+
+"Here we are."
+"here we are."
+
+
+
+"Let's go to shopping."
+
+"Let's go to shopping."
+ctg --> unitl next "g"
+"Let's go to shopping."
+
+
+"Let's go to shopping."
+cfg --> include next "g"
+"Let's go to shopping."
+
+"Let's go to shopping."
+cfg --> include next "g"
+"Let's go to shopping."
+2cfg --> include next "g",do twice
+"Let's go to shopping."
+
+"Let's go to shopping."
+
+cf'
+"Let's go to shopping."
+c/to
+"Let's go to shopping."
+"Let's go to shopping."
+"Let's go to shopping."
+"Let's go to shopping."
+"Let's go to shopping."
+
+cf'
+"Let's go to shopping."
+c/to
+"Let's go to shopping."
+"Let's go to shopping."
+"Let's go to shopping."
+"Let's go to shopping."
+"Let's go to shopping."
+"Let's go to shopping."
+"Let's go to shopping."
+"Let's go to shopping."
+"Let's go to shopping."
+
+							"Let's go to shopping."
+														 "Let's go to shopping."
+"Let's go to shopping."
+"Let's go to shopping."
+"Let's go to shopping."
---
+++ vim/vim_traning_draft.md
@@ -0,0 +1,70 @@
+# vim_traning_draft.md
+- Maintainer: yuan <eric@email.com>
+-    Created: 2020-10-14
+- LastChange: 2020-10-14
+-    Version: v0.0.01
+
+## syntax
+
+e.g operation + noun
+d(elete)
+w(word)
+type "dw" continuioustypely
+
+### actions in vim
+	- d(elete)
+	- c(hange)
+	- y(ank) means copy
+	- p(aste)
+	- u(ndo)
+	- v(isual select)
+	- > means indent to right
+	- < means indent to left
+	
+	- ds (tDelete Surrounding)
+	- cs (Change Surrounding)
+
+### nouns in vim
+#### motion
+	- w (forward by a "word")
+	- b(ack by a "word")
+	- 2j => down 2 lines
+#### text objects
+	- iw means "inner word"
+	- i" means "inner quotes"
+	- ip means "inner paragraph"
+	- as means "a sentence"
+	- it means "inner tag for HTML"
+#### parameterized text objects(search the postion)
+	- f,F
+	  - t,T
+		- \
+
+#### relativenumber
+:set relativenumber
+:set norelativenumber
+
+
+
+## repeat last action
+```
+## just type dot , any question? yes, just type "."
+.
+
+```
+
+
+### Reference:
+[Master the Vim Language](https://www.youtube.com/watch?v=wlR5gYd6um0)
+
+### Book
+	- Modern Vim: Craft Your Development Environment with Vim 8 and Neovim
+
+## plugin
+
+#### sourround
+
+``` shell
+cs"<  # 成对替换双引号成尖括号
+```
+
