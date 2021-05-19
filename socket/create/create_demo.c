5e96ea8d73e1a10ab6d9545ebacc19c11ed75d25
jinyuan <yuan.jin@beantechs.com>
2021-05-19 17:31:34 +0800

    add socket

---
+++ socket/create/create_demo.c
@@ -0,0 +1,27 @@
+/:q
+
+ * Filename: create_demo.c
+ *
+ * Copyright (C) 2018-2023 yuan  <yuan@company.com>. All Rights Reserved.
+ *
+ * History:
+ *    2021/03/19 - [yuan] Created file
+ *
+ * Maintainer: yuan <yuan@email.com>
+ *    Created: 2021-03-19
+ * LastChange: 2021-03-19
+ *    Version: v0.0.01
+ *
+ */
+#include <sys/socket.h>
+
+
+socket(AF_UNIX)
+	bind
+	listen
+	accpet
+	connect
+
+
+	recvfrom
+	sendto
